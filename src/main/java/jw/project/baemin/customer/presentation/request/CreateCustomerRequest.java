190997ccbbdb1b75377b33cac43fbdc1b8ca762d
유지웅 <poujis0@gmail.com>
2023-07-13 18:56:39 +0900

    [feat] Request To Entity 구현

--- src/main/java/jw/project/baemin/customer/presentation/request/CreateCustomerRequest.java
+++ src/main/java/jw/project/baemin/customer/presentation/request/CreateCustomerRequest.java
@@ -1,9 +1,17 @@
 package jw.project.baemin.customer.presentation.request;
 
+import jw.project.baemin.customer.domain.Customer;
+
 public record CreateCustomerRequest(
     String email,
     String name,
     String password
 ) {
-
+    public Customer toEntity(){
+        return new Customer(
+            this.email,
+            this.name,
+            this.password
+        );
+    }
 }
