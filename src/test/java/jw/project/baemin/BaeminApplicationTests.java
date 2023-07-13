74029dff72ffbb200c04de1340a0cb4ec7758a7e
유지웅 <poujis0@gmail.com>
2023-07-13 16:43:50 +0900

    first commit

---
+++ src/test/java/jw/project/baemin/BaeminApplicationTests.java
@@ -0,0 +1,13 @@
+package jw.project.baemin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
