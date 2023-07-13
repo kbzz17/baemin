f2be4e611d09ca7e59fb41671078e021136343ee
유지웅 <poujis0@gmail.com>
2023-07-13 18:58:49 +0900

    [feat] Entity To Response 구현

--- src/main/java/jw/project/baemin/customer/presentation/response/CustomerResponse.java
+++ src/main/java/jw/project/baemin/customer/presentation/response/CustomerResponse.java
@@ -1,4 +1,10 @@
 package jw.project.baemin.customer.presentation.response;
 
+import jw.project.baemin.customer.domain.Customer;
+
 public record CustomerResponse(String email, String name) {
+
+    public static CustomerResponse from(Customer customer) {
+        return new CustomerResponse(customer.getEmail(), customer.getName());
+    }
 }
