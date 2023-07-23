6dee872cf2d1eec2c3b975bca90ca1121a39195a
유지웅 <poujis0@gmail.com>
2023-07-23 16:38:19 +0900

    [feat] RegisterCoupon[Test] 구현

--- src/test/java/jw/project/baemin/customer/application/CouponServiceTest.java
+++ src/test/java/jw/project/baemin/customer/application/CouponServiceTest.java
@@ -3,15 +3,20 @@ package jw.project.baemin.customer.application;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 import jw.project.baemin.customer.domain.Coupon;
+import jw.project.baemin.customer.domain.Customer;
 import jw.project.baemin.customer.domain.enums.CouponType;
 import jw.project.baemin.customer.infrastructure.CouponRepository;
+import jw.project.baemin.customer.infrastructure.CustomerRepository;
 import jw.project.baemin.customer.presentation.request.coupon.CreateCouponRequest;
 import jw.project.baemin.customer.presentation.response.coupon.CouponResponse;
+import jw.project.baemin.support.CouponSupport;
+import jw.project.baemin.support.CustomerSupport;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,21 +40,16 @@ public class CouponServiceTest {
     @MockBean
     CouponRepository couponRepository;
 
+    @MockBean
+    CustomerRepository customerRepository;
+
     @Test
     @DisplayName("쿠폰 생성 기능 테스트")
     void createCouponTest() {
         CreateCouponRequest request = new CreateCouponRequest("말복쿠폰", CouponType.FIXED, 3000,
-            LocalDateTime.now());
+            LocalDateTime.now().withYear(2024));
 
-        Coupon coupon = Coupon.builder()
-            .id(1L)
-            .couponType(CouponType.FIXED)
-            .name("말복쿠폰")
-            .code("100101")
-            .expiredDate(LocalDateTime.now())
-            .discountAmount(3000)
-            .isUse(false)
-            .build();
+        Coupon coupon = CouponSupport.get(1L);
 
         given(couponRepository.save(any())).willReturn(coupon);
 
@@ -57,5 +57,22 @@ public class CouponServiceTest {
 
         assertThat(savedCoupon).extracting("name", "discountAmount", "couponType")
             .contains("말복쿠폰", 3000, CouponType.FIXED);
+    }
+
+    @Test
+    @DisplayName("쿠폰 등록 기능 테스트")
+    void registerCouponTest() {
+        Customer customer = CustomerSupport.get(1L);
+        Coupon coupon = CouponSupport.get(1L);
+
+        given(customerRepository.findById(anyLong())).willReturn(Optional.of(customer));
+        given(couponRepository.findByCode(anyString())).willReturn(Optional.of(coupon));
+        given(customerRepository.save(any())).willReturn(customer);
+
+        CouponResponse result = couponService.registerCoupon(1L, "100101");
+
+        assertThat(result).extracting("id", "couponType", "name", "code", "expiredDate",
+                "discountAmount")
+            .contains(1L, CouponType.FIXED, "말복쿠폰", "100101", coupon.getExpiredDate(), 3000);
     }
 }
