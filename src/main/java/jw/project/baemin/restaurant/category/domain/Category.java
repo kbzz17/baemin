399bf9ffae9ea24f71f41d50bd088a189cdb733d
유지웅 <poujis0@gmail.com>
2023-07-23 19:40:43 +0900

    [feat] Category[Entity] 구현

---
+++ src/main/java/jw/project/baemin/restaurant/category/domain/Category.java
@@ -0,0 +1,27 @@
+package jw.project.baemin.restaurant.category.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+}
