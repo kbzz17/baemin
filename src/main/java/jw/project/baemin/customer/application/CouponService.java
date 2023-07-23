2a448db4d3edf6c759dc2280b59ce1b1a5da69ff
유지웅 <poujis0@gmail.com>
2023-07-23 16:38:08 +0900

    [feat] RegisterCoupon[Service, Repository] 구현

--- src/main/java/jw/project/baemin/customer/application/CouponService.java
+++ src/main/java/jw/project/baemin/customer/application/CouponService.java
@@ -1,8 +1,11 @@
 package jw.project.baemin.customer.application;
 
 import jw.project.baemin.customer.domain.Coupon;
+import jw.project.baemin.customer.domain.Customer;
 import jw.project.baemin.customer.infrastructure.CouponRepository;
+import jw.project.baemin.customer.infrastructure.CustomerRepository;
 import jw.project.baemin.customer.presentation.request.coupon.CreateCouponRequest;
+import jw.project.baemin.customer.presentation.response.CustomerResponse;
 import jw.project.baemin.customer.presentation.response.coupon.CouponResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,10 +16,28 @@ public class CouponService {
 
     private final CouponRepository couponRepository;
 
+    private final CustomerRepository customerRepository;
 
     public CouponResponse createCoupon(CreateCouponRequest request) {
         Coupon coupon = request.toEntity();
+        if (coupon.hasExpired()){
+            throw new RuntimeException();
+        }
         coupon = couponRepository.save(coupon);
+        return CouponResponse.from(coupon);
+    }
+
+    public CouponResponse registerCoupon(Long customerId, String code) {
+        Customer customer = customerRepository.findById(customerId)
+            .orElseThrow(RuntimeException::new);
+        Coupon coupon = couponRepository.findByCode(code).orElseThrow(RuntimeException::new);
+
+        if (coupon.hasExpired()) {
+            throw new RuntimeException("만료된 쿠폰입니다.");
+        }
+        customer.addCoupon(coupon);
+        customerRepository.save(customer);
+
         return CouponResponse.from(coupon);
     }
 }
--- src/main/java/jw/project/baemin/customer/infrastructure/CouponRepository.java
+++ src/main/java/jw/project/baemin/customer/infrastructure/CouponRepository.java
@@ -1,8 +1,9 @@
 package jw.project.baemin.customer.infrastructure;
 
+import java.util.Optional;
 import jw.project.baemin.customer.domain.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
-
+    Optional<Coupon> findByCode(String code);
 }
